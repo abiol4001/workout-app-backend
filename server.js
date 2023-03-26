97746db1bad351aa70bbbe25c7e585c8ad514139
Abiola Oyewale <abiol4001@gmail.com>
2023-03-26 18:51:37 +0100

    Added CORS origin

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user")
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://gymflow.onrender.com/",
+  })
+);
 
 // routes
 app.use("/api/workouts", workoutRoutes);
